ba3224a4c700844f7109cbc05cb48f6aaff06586
Andy Olarte <andy.olarte514@gmail.com>
2019-10-23 14:07:05 -0500

    remove useless method

--- WBooks/StartingScreen/StartingScreenController.swift
+++ WBooks/StartingScreen/StartingScreenController.swift
@@ -21,9 +21,4 @@ class StartingScreenController: UIViewController {
         
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 }
