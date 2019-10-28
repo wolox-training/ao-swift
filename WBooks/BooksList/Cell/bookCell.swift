c27a1ced7c498c43834675e1e6fe08abe1d4e322
Andy Olarte <andy.olarte514@gmail.com>
2019-10-28 15:43:27 -0500

    creation cell book

---
+++ WBooks/BooksList/Cell/bookCell.swift
@@ -0,0 +1,24 @@
+//
+//  bookCell.swift
+//  WBooks
+//
+//  Created by Andy Francisco Olarte Cardona on 28/10/19.
+//  Copyright © 2019 Wolox. All rights reserved.
+//
+
+import UIKit
+
+class bookCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
