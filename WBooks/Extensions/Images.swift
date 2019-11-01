9541cfe01f915a49e9a53240a5d596affd03c572
Andy Olarte <andy.olarte514@gmail.com>
2019-11-01 11:15:04 -0500

    configure tab bar navigator

--- WBooks/Extensions/Images.swift
+++ WBooks/Extensions/Images.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 extension UIImage {
+    
+    // MARK: Icons books
+    
     public static var bookLitleBird: UIImage {
         return UIImage(named: "book_1")!
     }
@@ -29,6 +32,8 @@ extension UIImage {
         return UIImage(named: "book_5")!
     }
     
+    //MARK: Icons Nav Bar
+    
     public static var backgroundNavBar: UIImage {
         return UIImage(named: "bc_nav_bar")!
     }
@@ -43,6 +48,48 @@ extension UIImage {
     
     public static var iconBack: UIImage {
         return UIImage(named: "ic_back")!
+    }
+    
+    //MARK: Icons Tab Bar
+    
+    public static var iconAddNewActive: UIImage {
+        return UIImage(named: "ic_add_new_active")!
+    }
+    
+    public static var iconAddNewInactive: UIImage {
+        return UIImage(named: "ic_add_new")!
+    }
+    
+    public static var iconLibraryActive: UIImage {
+        return UIImage(named: "ic_library_active")!
+    }
+    
+    public static var iconLibraryInactive: UIImage {
+        return UIImage(named: "ic_library")!
+    }
+    
+    public static var iconRentalsActive: UIImage {
+        return UIImage(named: "ic_myrentals_active")!
+    }
+    
+    public static var iconRentalsInactive: UIImage {
+        return UIImage(named: "ic_myrentals")!
+    }
+    
+    public static var iconSettingsActive: UIImage {
+        return UIImage(named: "ic_settings_active")!
+    }
+    
+    public static var iconSettingsInactive: UIImage {
+        return UIImage(named: "ic_settings")!
+    }
+    
+    public static var iconStarActive: UIImage {
+        return UIImage(named: "ic_wishlist_active")!
+    }
+    
+    public static var iconStarInactive: UIImage {
+        return UIImage(named: "ic_wishlist")!
     }
 
 }
---
+++ WBooks/Navigators/TabBarController.swift
@@ -0,0 +1,41 @@
+//
+//  TabBarController.swift
+//  WBooks
+//
+//  Created by Andy Francisco Olarte Cardona on 1/11/19.
+//  Copyright © 2019 Wolox. All rights reserved.
+//
+
+import UIKit
+import WolmoCore
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTabBar()
+    }
+    
+    private func configureTabBar() {
+        tabBar.barTintColor = .backgroundPolar()
+
+        let tabButtonLibrary = NavigationBarCotroller(rootViewController: BookListController())
+        tabButtonLibrary.tabBarItem = UITabBarItem.tabButtonLibrary
+        
+        let tabButtonWishlist = NavigationBarCotroller(rootViewController: BookListController())
+        tabButtonWishlist.tabBarItem = UITabBarItem.tabButtonWishlist
+        
+        let tabButtonSuggest = NavigationBarCotroller(rootViewController: BookListController())
+        tabButtonSuggest.tabBarItem = UITabBarItem.tabButtonSuggest
+        
+        let tabButtonRentals = NavigationBarCotroller(rootViewController: BookListController())
+        tabButtonRentals.tabBarItem = UITabBarItem.tabButtonRentals
+        
+        let tabButtonSettings = NavigationBarCotroller(rootViewController: BookListController())
+        tabButtonSettings.tabBarItem = UITabBarItem.tabButtonSettings
+        
+        viewControllers = [tabButtonLibrary, tabButtonWishlist, tabButtonSuggest, tabButtonRentals, tabButtonSettings]
+        
+    }
+
+}
