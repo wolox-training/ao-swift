5cac85431bad1d8af74aad75afc9687850eab3e4
Andy Olarte <andy.olarte514@gmail.com>
2019-11-01 11:29:54 -0500

    adjust titles

--- WBooks/Extensions/TabBarButtons.swift
+++ WBooks/Extensions/TabBarButtons.swift
@@ -11,31 +11,31 @@ import UIKit
 extension UITabBarItem {
     
     public static var tabButtonLibrary: UITabBarItem {
-        let tabButtonLibrary = UITabBarItem(title: "LIBRARY_TITLE".localized(), image: UIImage.iconLibraryInactive, selectedImage: UIImage.iconLibraryActive)
+        let tabButtonLibrary = UITabBarItem(title: "LIBRARY".localized(), image: UIImage.iconLibraryInactive, selectedImage: UIImage.iconLibraryActive)
         tabButtonLibrary.tag = 0
         return tabButtonLibrary
     }
     
     public static var tabButtonWishlist: UITabBarItem {
-        let tabButtonWishlist = UITabBarItem(title: "WISHLIST_TITLE".localized(), image: UIImage.iconStarInactive, selectedImage: UIImage.iconStarActive)
+        let tabButtonWishlist = UITabBarItem(title: "WISHLIST".localized(), image: UIImage.iconStarInactive, selectedImage: UIImage.iconStarActive)
         tabButtonWishlist.tag = 1
         return tabButtonWishlist
     }
     
     public static var tabButtonSuggest: UITabBarItem {
-        let tabButtonSuggest = UITabBarItem(title: "SUGGEST_TITLE".localized(), image: UIImage.iconAddNewInactive, selectedImage: UIImage.iconAddNewActive)
+        let tabButtonSuggest = UITabBarItem(title: "SUGGEST".localized(), image: UIImage.iconAddNewInactive, selectedImage: UIImage.iconAddNewActive)
         tabButtonSuggest.tag = 2
         return tabButtonSuggest
     }
     
     public static var tabButtonRentals: UITabBarItem {
-        let tabButtonRentals = UITabBarItem(title: "RENTALS_TITLE".localized(), image: UIImage.iconRentalsInactive, selectedImage: UIImage.iconRentalsActive)
+        let tabButtonRentals = UITabBarItem(title: "RENTALS".localized(), image: UIImage.iconRentalsInactive, selectedImage: UIImage.iconRentalsActive)
         tabButtonRentals.tag = 3
         return tabButtonRentals
     }
     
     public static var tabButtonSettings: UITabBarItem {
-        let tabButtonSettings = UITabBarItem(title: "SETTINGS_TITLE".localized(), image: UIImage.iconSettingsInactive, selectedImage: UIImage.iconSettingsActive)
+        let tabButtonSettings = UITabBarItem(title: "SETTINGS".localized(), image: UIImage.iconSettingsInactive, selectedImage: UIImage.iconSettingsActive)
         tabButtonSettings.tag = 4
         return tabButtonSettings
     }
